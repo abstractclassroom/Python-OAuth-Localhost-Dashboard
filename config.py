1c565d8ce6362cedc7dbb3798e4bf3342749e972
Nathan J. Russell <6741335+nathanjrussell@users.noreply.github.com>
2026-06-03 09:08:40 -0400

    Change GITHUB_CLIENT_SECRET value
    Updated GITHUB_CLIENT_SECRET for security reasons.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BASE_URL = "https://8jqr6mgxui.execute-api.us-east-1.amazonaws.com/Production"
 GITHUB_CLIENT_ID = "Ov23liwh1QkRa3OqHQoL"
-GITHUB_CLIENT_SECRET = "309b0114092acdd4e2f5a2f726ead4473cf7c937"
+GITHUB_CLIENT_SECRET = "031d31bede8dfe539858e6743f7719217c68e8d8"
 STATE_SECRET = "change-me"
 
